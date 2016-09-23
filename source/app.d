6ccfafa5052aa101266502ed5324daaf239447c7
dushibaiyu <dushibaiyu@yahoo.com>
2016-09-23 11:47:42 +0800

    rename, and class to struct.

--- source/app.d
+++ source/app.d
@@ -2,14 +2,14 @@ import std.functional;
 import collie.codec.http.parser;
 import std.stdio;
 
-void on_message_begin(HTTPParser)
+void on_message_begin(ref HTTPParser)
 {
     writeln("_on_message_begin");
 
     writeln(" ");
 }
 
-void on_url(HTTPParser par, ubyte[] data, bool adv)
+void on_url(ref HTTPParser par, ubyte[] data, bool adv)
 {
     writeln("_on_url, is NOADVANCE = ", adv);
     writeln("\" ", cast(string) data, " \"");
@@ -17,14 +17,14 @@ void on_url(HTTPParser par, ubyte[] data, bool adv)
     writeln(" ");
 }
 
-void on_status(HTTPParser par, ubyte[] data, bool adv)
+void on_status(ref HTTPParser par, ubyte[] data, bool adv)
 {
     writeln("_on_status, is NOADVANCE = ", adv);
     writeln("\" ", cast(string) data, " \"");
     writeln(" ");
 }
 
-void on_header_field(HTTPParser par, ubyte[] data, bool adv)
+void on_header_field(ref HTTPParser par, ubyte[] data, bool adv)
 {
     static bool frist = true;
     writeln("_on_header_field, is NOADVANCE = ", adv);
@@ -39,39 +39,42 @@ void on_header_field(HTTPParser par, ubyte[] data, bool adv)
     writeln(" ");
 }
 
-void on_header_value(HTTPParser par, ubyte[] data, bool adv)
+void on_header_value(ref HTTPParser par, ubyte[] data, bool adv)
 {
     writeln("_on_header_value, is NOADVANCE = ", adv);
     writeln("\" ", cast(string) data, " \"");
     writeln(" ");
 }
 
-void on_headers_complete(HTTPParser par)
+void on_headers_complete(ref HTTPParser par)
 {
+	par.handleIng = false;
     writeln("_on_headers_complete");
     writeln(" ");
+
 }
 
-void on_body(HTTPParser par, ubyte[] data, bool adv)
+void on_body(ref HTTPParser par, ubyte[] data, bool adv)
 {
     writeln("_on_body, is NOADVANCE = ", adv);
     writeln("\" ", cast(string) data, " \"");
     writeln(" ");
 }
 
-void on_message_complete(HTTPParser par)
+void on_message_complete(ref HTTPParser par)
 {
     writeln("_on_message_complete");
     writeln(" ");
 }
 
-void on_chunk_header(HTTPParser par)
+void on_chunk_header(ref HTTPParser par)
 {
     writeln("_on_chunk_header");
     writeln(" ");
+	par.handleIng = false;
 }
 
-void on_chunk_complete(HTTPParser par)
+void on_chunk_complete(ref HTTPParser par)
 {
     writeln("_on_chunk_complete");
     writeln(" ");
@@ -81,7 +84,7 @@ void main()
 {
     string data = "GET /test HTTP/1.1\r\nUser-Agent: curl/7.18.0 (i486-pc-linux-gnu) libcurl/7.18.0 OpenSSL/0.9.8g zlib/1.2.3.3 libidn/1.1\r\nHo";
     string data2 = "st: 0.0.0.0=5000\r\nAccept: */*\r\n\r\n";
-    HTTPParser par = new HTTPParser();
+    HTTPParser par = HTTPParser(HTTPParserType.HTTP_BOTH);
     par.onMessageBegin = toDelegate(&on_message_begin);
     par.onMessageComplete = toDelegate(&on_message_complete);
     par.onUrl = toDelegate(&on_url);
